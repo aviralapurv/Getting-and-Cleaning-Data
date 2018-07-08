c7fec95f87fe45bcfe8cc52cda55cd0f83d788b7
Aviral Apurva <aviralapurv@gmail.com>
2018-07-08 12:17:43 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,43 +1,28 @@
-## Verifies that the required libraries get installed if needed
 verify_deps <- function(...) {
   lapply(list(...), function(lib) {
     if (!lib %in% installed.packages()) 
       install.packages(lib)
   })
 }
-
-## Verify dependencies
 verify_deps("dplyr", "reshape2")
-
-## Load the dependencies
 library("dplyr")
 library("reshape2")
-
-## The main data directory
 ds_dir <- "UCI HAR Dataset"
 
-## Loads a file
-## Params:
-##  filename .. name of the file to load
-##  ellipsis .. a comma delimited list of directories
+
 load_file <- function(filename, ...) {
   file.path(..., filename) %>%
   read.table(header = FALSE)
 }
 
-## Loads a training file
 load_train_file <- function(filename) {
   load_file(filename, ds_dir, "train")
 }
 
-## Loads a test file
 load_test_file <- function(filename) {
   load_file(filename, ds_dir, "test")
 }
 
-## Uses list of activity values to describe test / training labels
-## Params: ds .. label dataset
-## Returns: the original dataset with human readable column name and values
 describe_lbl_ds <- function(ds) {
   names(ds) <- activity_col  
   ds$Activity <- factor(ds$Activity, levels = activity_lbl$V1, labels = activity_lbl$V2)
@@ -54,13 +39,11 @@ describe_act_ds <- function(ds) {
   ds
 }
 
-## Adjusts column name in the data set identifying test participants
 describe_sub_ds <- function(ds) {
   names(ds) <- subject_col
   ds
 }
 
-## Download and extract a zip file with datasets
 if (!file.exists(ds_dir)) {
   source_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   dest_file <- "Dataset.zip"
@@ -74,25 +57,19 @@ if (!file.exists(ds_dir)) {
 subject_col <- "Subject"
 activity_col <- "Activity"
 
-## Load features as human-readable column names
 features <- load_file("features.txt", ds_dir)
 
-## Load activity labels
 activity_lbl <- load_file("activity_labels.txt", ds_dir)
 
-## Use descriptive activity names to name the activities in the data set
-#### Training data
+
 train_set <- load_train_file("X_train.txt") %>% describe_act_ds
 train_lbl <- load_train_file("y_train.txt") %>% describe_lbl_ds
 train_sub <- load_train_file("subject_train.txt") %>% describe_sub_ds
 
-#### Test data
 test_set <- load_test_file("X_test.txt") %>% describe_act_ds
 test_lbl <- load_test_file("y_test.txt") %>% describe_lbl_ds
 test_sub <- load_test_file("subject_test.txt") %>% describe_sub_ds
 
-## Merge the training and the test sets to create one dataset
-## Extract only the measurements on the mean and standard deviation for each measurement
 merge_data <- rbind(
                 cbind(train_set, train_lbl, train_sub),
                 cbind(test_set, test_lbl, test_sub)
@@ -101,8 +78,6 @@ merge_data <- rbind(
                 matches("mean|std"), 
                 one_of(subject_col, activity_col)
               )
-
-## Create a second, independent tidy data set with the average of each variable for each activity and each subject
 id_cols <- c(subject_col, activity_col)
 tidy_data <- melt(
                merge_data, 
@@ -111,9 +86,7 @@ tidy_data <- melt(
              ) %>%
              dcast(Subject + Activity ~ variable, mean)
              
-## Save the result
-### 'txt' is a requirement
+
 write.table(tidy_data, file = "tidy_data.txt", sep = ",", row.names = FALSE)
 
-### 'csv' is an addition: it's nicely formatted on GitHub and can be easily imported into a spreadsheet
 write.table(tidy_data, file = "tidy_data.csv", sep = ",", row.names = FALSE)
